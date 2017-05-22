4c20ab128be5cb326da4563eee95c742e62e03d8
Branko Straub <b.straub@outlook.com>
2017-05-22 13:06:26 -0300

    Commit VS

---
+++ CalzadaWeb/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CalzadaWeb.Models;
+
+namespace CalzadaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Formulario model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("","Revise los campos del formulario");
+                return View("Index", model);
+            }
+
+            var mailMsg = new MailMessage();
+            mailMsg.To.Add(new MailAddress("recepcion.admyebra@gmail.com", "ADM Yebra"));
+            mailMsg.From = new MailAddress(model.Email, model.Name);
+            mailMsg.Subject = "Nuevo Formulario - General";
+                  
+                  
+
+            try
+            { 
+              
+                var html = $"<p>Nombre: {model.Name} </p>" +
+                    $"<p>Email: {model.Email} </p>" +
+                    $"<p>Telefono: {model.Phone} </p>" +
+                    $"<p>Comentarios: {model.Message} </p>";
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+                var smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+                var credentials = new NetworkCredential("azure_247112f2851e96ce1624b2ebf88501cd@azure.com", "Nbt101296#");
+                smtpClient.Credentials = credentials;
+                smtpClient.Send(mailMsg);
+            }
+
+            catch (Exception ex)
+            {
+            Console.WriteLine(ex.Message);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+       
+    }
+}
---
+++ CalzadaWeb/Models/Formulario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CalzadaWeb.Models
+{
+    public class Formulario
+    {
+        [Required(ErrorMessage = "El nombre es un campo requerido")]
+        public string Name { get; set; }
+
+      
+        [Required(ErrorMessage = "El mail es un campo requerido")]
+        [EmailAddress(ErrorMessage = "Direccion de correo invalida")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El mensaje es un campo requerido")]
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "El telefono es un campo requerido")]
+        [Phone(ErrorMessage = "Numero de telefono invalido")]
+        public string Phone { get; set; }
+
+     
+    }
+}
